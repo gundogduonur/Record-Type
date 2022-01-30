f1c155d39cf5e6750cd65f5cb4f8148778c38c0e
Onur Gündoğdu <gundogdusoftware@outlook.com>
2022-01-30 17:09:50 +0300

    Objects

--- RecordType/src/com/onurgundogdu/main/Main.java
+++ RecordType/src/com/onurgundogdu/main/Main.java
@@ -5,6 +5,12 @@ import com.onurgundogdu.record.Student;
 public class Main {
     public static void main(String[] args) {
         Student s1=new Student(1L,"Onur","Gündoğdu");
+        Student s2=new Student(2L,"Gündoğdu","Onur");
+        Student s3=new Student(3L,"","");
         System.out.println(s1.id()+" "+s1.firstname()+" "+s1.lastname());
+        System.out.println(s1);
+        System.out.println(s1.hashCode());
+        System.out.println(s1.equals(s2));
+        System.out.println(s3);
     }
 }
--- RecordType/src/com/onurgundogdu/record/Student.java
+++ RecordType/src/com/onurgundogdu/record/Student.java
@@ -1,4 +1,6 @@
 package com.onurgundogdu.record;
 
 public record Student(Long id,String firstname,String lastname) {
+
+
 }
